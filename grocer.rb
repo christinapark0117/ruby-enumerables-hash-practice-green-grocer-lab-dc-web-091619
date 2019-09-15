b4ffa34be0bea001326d58bf3c516956a5c9819c
Christina Park <christinapark0117@gmail.com>
2019-09-15 06:31:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -15,7 +15,27 @@ end
 def apply_coupons(cart, coupons)
   coupons.each do |coupon_hash|
     
+    item = coupon[:item]
+
+    if cart[item] && coupon_hash[:item][:count] >= coupon_hash[:num]
+      temp = {"#{item} W/COUPON" => {
+        :price => coupon_hash[:cost],
+        :clearance => hash[item][:clearance],
+        :count => 1
+        }
+      }
+      
+      if hash["#{item} W/COUPON"].nil?
+        hash.merge!(temp)
+      else
+        hash["#{item} W/COUPON"][:count] += 1
+       
+      end
+      
+      hash[item][:count] -= coupon_hash[:num]
+    end
   end
+  hash
 end
 
 def apply_clearance(cart)
