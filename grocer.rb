852587e79bdd8be049b123ab98d46f8192a6e78d
Christina Park <christinapark0117@gmail.com>
2019-09-15 06:26:34 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,21 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart ={}
+  count = 0 
+  
+  cart.each do |element|
+    element.each do |item, hash|
+      new_cart[item] ||= hash
+      new_cart[item][:count] ||= 0
+      new_cart[item][:count] += 1
+    end
+  end
+  new_cart
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  coupons.each do |coupon_hash|
+    
+  end
 end
 
 def apply_clearance(cart)
