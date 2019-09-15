6bf6a6dcd13a717fc8cb53c77390becb88ad26c4
Christina Park <christinapark0117@gmail.com>
2019-09-15 07:41:46 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -20,7 +20,7 @@ def apply_coupons(cart, coupons)
     if cart[item]
       if cart[item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
         cart["#{item} W/COUPON"] = {price: coupon[:cost] / coupon[:num ], clearance: cart[item][:clearance], count: coupon[:num]}
-        cart[item][:count] -= coupon[:num]
+          cart[item][:count] -= coupon[:num]
     elsif
       cart[item][:count] >= coupon[:num] && cart["#{item} W/COUPON"]
       cart["#{item} W/COUPON"][:count] += coupon[:num]
@@ -39,12 +39,4 @@ def apply_clearance(cart)
   cart
 end
 
-def checkout(cart, coupons)
-  new_cart = consolidate_cart(cart)
-  coupon_cart = apply_coupons(new_cart, coupons)
-  clearance_cart = apply_clearance(coupon_cart)
-  
-  total = clearance_cart.reduce(0){|acc, (key, value)| acc += value[:price] * value[:count]
-  
-  total > 100? total * 0.9 : total
-end
+
