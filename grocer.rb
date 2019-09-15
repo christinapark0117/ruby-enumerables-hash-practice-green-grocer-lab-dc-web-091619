d774232080285f86d393d6e9c2e6419b1308c1c4
Christina Park <christinapark0117@gmail.com>
2019-09-15 06:38:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,7 +18,7 @@ def apply_coupons(cart, coupons)
     item = coupon[:item]
 
     if cart[item] && coupon_hash[:item][:count] >= coupon[:num] && !cart["#{item} W/COUPON"]
-      cart["#{item} W/COUPON"]
+      cart["#{item} W/COUPON"] = {}
       
     end  
   end
